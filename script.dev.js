7e9856e764a76b6558ff62991654339dce2fe007
duc12052001 <96016484+duc12052001@users.noreply.github.com>
2021-12-12 23:33:34 +0700

    add MyWebsite file

---
+++ script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var typed = new Typed(".typing", {
+  strings: ["", "Vật Lý Kỹ Thuật", "Đại học Bách Khoa", "Vật Lý Kỹ Thuật", "Đại học Bách Khoa"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
+//# sourceMappingURL=script.dev.js.map
